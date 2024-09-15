f53d7e746cbb6b026913a38c319a30d4a804e43e
ABDALHAMID <ABD.VOU@GMAIL.COM>
2024-09-15 22:24:42 +0100

    create new parcels and delete

--- app/routes/parcel_routes.py
+++ app/routes/parcel_routes.py
@@ -14,8 +14,9 @@ def edit_parcel(parcel_id):
     print(f'Edit parcel with ID {parcel_id} in parcel service')
     return render_template('edit_parcel.html', parcel_id=parcel_id)
 
-@parcel_bp.route('/delete_parcel/<int:parcel_id>')
-def delete_parcel(parcel_id):
+@parcel_bp.route('/delete_parcel', methods=['POST'])
+def delete_parcel():
+    parcel_id = request.form['parcel_id']
     print(f'Delete parcel with ID {parcel_id} in parcel service')
     ParcelService.delete_parcel(parcel_id)
     parcels = ParcelService.get_all_parcels()
@@ -25,3 +26,15 @@ def delete_parcel(parcel_id):
 def new_parcel():
     print('Create new parcel in parcel service')
     return render_template('new_parcel.html')
+
+@parcel_bp.route('/create', methods=['POST'])
+def create_parcel():
+
+    reference = request.form['reference'],
+    description = request.form['description'],
+    status = request.form['status'],
+    
+    new_parcel = ParcelService.create_parcel(reference, description, status)
+
+    flash('Parcel created successfully!')
+    return list_parcels()
